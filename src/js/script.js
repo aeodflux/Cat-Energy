65412000c739c67814674f52c921132a5bd781aa
silkesssji <kastrdast@mail.ru>
2021-08-15 16:24:32 +0200

    Add offers

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,22 @@
-var menuOpen = document.querySelector(".j-menu-open");
-var navMobile = document.querySelector(".nav__menu");
-var itemMobile = document.querySelector(".nav__menu");
+var toggleMobileMenuButton = document.querySelector(".nav__toggle-menu");
+var nav = document.querySelector(".header");
+var menu = document.querySelector(".nav__menu");
+var x = false;
 
-menuOpen.addEventListener("click", function(evt) {
+toggleMobileMenuButton.addEventListener("click", function(evt) {
     evt.preventDefault();
-    navMobile.classList.toggle("nav__menu-open")
-    itemMobile.classList.add("menu-mobile")
-    menuOpen.classList.toggle("j-menu-close")
-});
-var menuClose = document.querySelector(".j-menu-close");
-menuClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    // navMobile.classList.remove("nav__menu-open")
-    itemMobile.classList.remove("menu-mobile")
+    if (x === true) {
+        nav.classList.add("nav--opened")
+        menu.classList.remove("move-up")
+        menu.classList.add("move-down")
+        x = false;
+      } else {
+        menu.classList.remove("move-down")
+        menu.classList.add("move-up")
+        x = true;
+        if (x == false) {
+        nav.classList.remove("nav--opened")
+        }
+      }
 });
 
