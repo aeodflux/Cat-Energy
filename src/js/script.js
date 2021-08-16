8c14ff736cf8d407a3f845a64f99f7602f9dcf62
silkesssji <kastrdast@mail.ru>
2021-08-16 11:29:06 +0200

    Fix the animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,20 @@
 var toggleMobileMenuButton = document.querySelector(".nav__toggle-menu");
-var nav = document.querySelector(".header");
+var nav = document.querySelector(".nav");
 var menu = document.querySelector(".nav__menu");
-var x = false;
 
+var isMenuOpened = false;
+var closingTimeout;
 toggleMobileMenuButton.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    if (x === true) {
-        nav.classList.add("nav--opened")
-        menu.classList.remove("move-up")
-        menu.classList.add("move-down")
-        x = false;
-      } else {
-        menu.classList.remove("move-down")
-        menu.classList.add("move-up")
-        x = true;
-        if (x == false) {
-        nav.classList.remove("nav--opened")
-        }
-      }
+  evt.preventDefault();
+  if (!isMenuOpened) {
+    clearTimeout(closingTimeout)
+    nav.classList.add("nav--opened")
+    menu.classList.remove("nav__menu--animation--closing")
+    menu.classList.add("nav__menu--animation--opening")
+  } else {
+    menu.classList.remove("nav__menu--animation--opening")
+    menu.classList.add("nav__menu--animation--closing")
+    closingTimeout = setTimeout(() => nav.classList.remove("nav--opened"), 800)
+  }
+  isMenuOpened = !isMenuOpened
 });
-
